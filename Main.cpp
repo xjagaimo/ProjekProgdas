b7f4f80ecdf8f8d5072a7383cfea7bb0760ba9ca
xjagaimo <92840347+xjagaimo@users.noreply.github.com>
2021-11-25 20:36:20 +0700

    updet gan

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,26 @@
-#include <iostream>
+#include<iostream>
+using namespace std;
 
+class item {
+	public:
+	string nama;
+	int harga;
+};
+	
 int main() {
-    
-    return 0;
+	item benda[5];
+	
+	for (int i=0; i<5; i++) {
+	cout << "Nama: " ;
+	cin >> benda[i].nama;
+	cout << "Harga: ";
+	cin >> benda[i].harga; 
+	}
+	
+	for (int i=0; i<5; i++) {
+	
+	cout << benda[i].nama << endl;
+	cout << benda[i].harga << endl; }
+	
+return 0; 
 }
