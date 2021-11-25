7083f3c3c6fd33197fdbb9932551a865f12aae97
xjagaimo <92840347+xjagaimo@users.noreply.github.com>
2021-11-25 21:02:29 +0700

    updet 2

--- Main.cpp
+++ Main.cpp
@@ -6,24 +6,40 @@ class item {
 	string nama;
 	int harga;
 };
+
+item benda[3];
+
+void display() {
+	for (int i=0; i<3; i++) { // display
+	cout << benda[i].nama << endl;
+	cout << benda[i].harga << endl; 
+	}
+}
 	
 int main() {
-	int jmlh;
-	cout << "Jumlah item yang diinginkan?";
-	cin >> jmlh;
-	item benda[jmlh];
 	
-	for (int i=0; i<=jmlh; i++) {
+	bool edit = 0;
+	int a;
+	
+	for (int i=0; i<3; i++) { //add item
 	cout << "Nama: " ;
 	cin >> benda[i].nama;
 	cout << "Harga: ";
 	cin >> benda[i].harga; 
 	}
-	
-	for (int i=0; i<5; i++) {
-	
-	cout << benda[i].nama << endl;
-	cout << benda[i].harga << endl; }
-	
+	display();
+		
+	cout << "mau ngedit gan? [true/false]";	// edit
+	cin >> edit;
+	if (edit) {
+	cout << "barang ke berapa gan?";
+	cin >> a;
+	cout << "Barang ke"<<a<<endl;
+	cout << "Nama: " ;
+	cin >> benda[a].nama;
+	cout << "Harga: ";
+	cin >> benda[a].harga;
+	display(); 
+	}
 return 0; 
 }
